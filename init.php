f5ba529fe594bb30aa67f651d46f6633e48529a9
SlaninaB <slanina.bretislav@zak.spspb.cz>
2026-03-16 09:19:20 +0100

    init.php

---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+$db = new PDO("sqlite:profile.db");
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$db->exec("CREATE TABLE IF NOT EXISTS interests (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+)");
